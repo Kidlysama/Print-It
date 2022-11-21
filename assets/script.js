f7c72e19e90eada6c14399b1001fe23257ca3ff6
kidlysama <kylli444@gmail.com>
2022-11-21 22:22:30 +0100

    m

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,23 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+current_index = 0;
+
+function carousel(i){
+    current_index += i;
+    if (current_index > 4) { current_index = 1; }
+    if (current_index < 1) { current_index = 4; }
+    document.getElementById('img_slide').src = "./assets/images/slideshow/"+slides[current_index - 1].image;
+    document.getElementById('tagline').innerHTML = slides[current_index - 1].tagLine;
+
+    for (var j = 0; j < 4; j++) {
+        document.getElementById('dots').children[j].className = "dot";
+        if (current_index - 1 == j) {
+            document.getElementById('dots').children[j].className = "dot dot_selected";
+        }
+    }
+
+}
+
+carousel(1);
